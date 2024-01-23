2fbf5970d072978fc6eb795f9c8ebd4252c1b3f8
MathematicalFunctionANUBHAV <anubhavvermadelhi@gmail.com>
2024-01-23 12:12:23 +0530

    step_2

---
+++ StaticGestures.py
@@ -0,0 +1,135 @@
+import csv
+import copy
+import argparse
+
+import itertools
+from collections import Counter
+from collections import deque
+
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+from utils import CvFpsCalc
+from models import KeyPointClassifier
+from utils.functions import calc_bounding_rect, calc_landmark_list, draw_bounding_rect, draw_info, draw_info_text, draw_landmarks, pre_process_landmark
+
+number = 1
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--width", help='cap width', type=int, default=960)
+    parser.add_argument("--height", help='cap height', type=int, default=540)
+
+    parser.add_argument('--use_static_image_load', action='store_true')
+    parser.add_argument("--min_detection_confidence", help= 'min_detection_confidence', type= float, default=0.7)
+    parser.add_argument("--min_tracking_confidence", help='min_tracking_confidence', type= int, default=0.5)
+
+    args = parser.parse_args()
+
+    return args
+def main():
+    #  Argument Parsing --------------------------------------------------------------------------------------------
+    args = get_args()
+
+    cap_device = args.device
+    cap_width = args.width
+    cap_height = args.height
+
+    use_static_image_mode = args.use_static_image_load
+    min_detection_confidence = args.min_detection_confidence
+    min_tracking_confidence = args.min_tracking_confidence
+
+    use_brect = True
+
+    #   Camera preparation -----------------------------------------------------------------------------------------
+
+    cap = cv.VideoCapture(cap_device)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
+
+    #   Loading the models -----------------------------------------------------------------------------------------
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(
+        static_image_mode = use_static_image_mode,
+        max_num_hands = 4, 
+        min_detection_confidence = min_detection_confidence,
+        min_tracking_confidence = min_tracking_confidence,
+    )
+
+    keypoint_classifier = KeyPointClassifier()
+
+    #   Read Labels ------------------------------------------------------------------------------------------------
+    with open('datasets\keypoint_classifier_label.csv', encoding='utf-8-sig') as f:
+        keypoint_classifier_labels = csv.reader(f)
+        keypoint_classifier_labels = [
+            row[0] for row in keypoint_classifier_labels
+        ]        
+    #    FPS Measurement -------------------------------------------------------------------------------------------
+    cvFpsCalc = CvFpsCalc(buffer_len=10)
+
+    # implementation 
+    while True:
+        fps = cvFpsCalc.get()
+
+        #process key (esc == 27: end)
+        key = cv.waitKey(10)
+        if key == 27:
+            break
+        # Camera capture
+        ret, image = cap.read()
+        if not ret:
+            break
+
+        image = cv.flip(image,1)
+        debug_image = copy.deepcopy(image)
+        # Detection Implementation
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+
+        #this is totally not working in the future but okay cool 
+        if results.multi_hand_landmarks is not None:
+            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                # Bounded Box Calculation
+                brect = calc_bounding_rect(debug_image, hand_landmarks)
+                # Landmark Calculation
+                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+
+                # Conversion to relative co-ordinates / normalized co-ordinates 
+                pre_processed_landmark_list = pre_process_landmark(
+                    landmark_list)
+                # Hand Sign Classification
+                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
+
+
+                #Drawing on the cam
+                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+                debug_image = draw_landmarks(debug_image, landmark_list)
+                debug_image = draw_info_text(
+                    debug_image,
+                    brect,
+                    handedness,
+                    keypoint_classifier_labels[hand_sign_id],
+                )
+        else:
+            pass
+
+        debug_image = draw_info(debug_image, fps, number, mode=0)
+
+        # Screen Name
+        cv.imshow('Static Hand Gestures Recognition', debug_image)
+    cap.release()
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
--- utils/functions.py
+++ utils/functions.py
@@ -1,135 +1,9 @@
-import csv
 import copy
-import argparse
-
+import csv
 import itertools
-from collections import Counter
-from collections import deque
-
-import cv2 as cv
 import numpy as np
-import mediapipe as mp
+import cv2 as cv
 
-from utils import CvFpsCalc
-from models import KeyPointClassifier
-
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--device", type=int, default=0)
-    parser.add_argument("--width", help='cap width', type=int, default=960)
-    parser.add_argument("--height", help='cap height', type=int, default=540)
-
-    parser.add_argument('--use_static_image_load', action='store_true')
-    parser.add_argument("--min_detection_confidence", help= 'min_detection_confidence', type= float, default=0.7)
-    parser.add_argument("--min_tracking_confidence", help='min_tracking_confidence', type= int, default=0.5)
-
-    args = parser.parse_args()
-
-    return args
-def main():
-    #  Argument Parsing --------------------------------------------------------------------------------------------
-    args = get_args()
-
-    cap_device = args.device
-    cap_width = args.width
-    cap_height = args.height
-
-    use_static_image_mode = args.use_static_image_load
-    min_detection_confidence = args.min_detection_confidence
-    min_tracking_confidence = args.min_tracking_confidence
-
-    use_brect = True
-
-    #   Camera preparation -----------------------------------------------------------------------------------------
-
-    cap = cv.VideoCapture(cap_device)
-    cap.set(cv.CAP_PROP_FRAME_WIDTH, cap_width)
-    cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
-
-    #   Loading the models -----------------------------------------------------------------------------------------
-    mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(
-        static_image_mode = use_static_image_mode,
-        max_num_hands = 1, 
-        min_detection_confidence = min_detection_confidence,
-        min_tracking_confidence = min_tracking_confidence,
-    )
-
-    keypoint_classifier = KeyPointClassifier()
-
-    #TODO PointHistoryClassification
-    #   Read Labels ------------------------------------------------------------------------------------------------
-    with open('datasets\keypoint_classifier_label.csv', encoding='utf-8-sig') as f:
-        keypoint_classifier_labels = csv.reader(f)
-        keypoint_classifier_labels = [
-            row[0] for row in keypoint_classifier_labels
-        ]
-    #TODO PointHistoryClassification
-        
-    #    FPS Measurement -------------------------------------------------------------------------------------------
-    cvFpsCalc = CvFpsCalc(buffer_len=10)
-
-    #    Co-ordinate history ---------------------------------------------------------------------------------------
-    history_length = 16
-    point_history = deque(maxlen=history_length)
-
-    # implementation 
-    mode = 0
-    while True:
-        fps = cvFpsCalc.get()
-
-        #process key (esc == 27: end)
-        key = cv.waitKey(10)
-        if key == 27:
-            break
-        # Camera capture
-        ret, image = cap.read()
-        if not ret:
-            break
-
-        image = cv.flip(image,1)
-        debug_image = copy.deepcopy(image)
-        # Detection Implementation
-        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-
-        image.flags.writeable = False
-        results = hands.process(image)
-        image.flags.writeable = True
-
-        #this is totally not working in the future but okay cool 
-        if results.multi_hand_landmarks is not None:
-            for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-                # Bounded Box Calculation
-                brect = calc_bounding_rect(debug_image, hand_landmarks)
-                # Landmark Calculation
-                landmark_list = calc_landmark_list(debug_image, hand_landmarks)
-
-                # Conversion to relative co-ordinates / normalized co-ordinates 
-                pre_processed_landmark_list = pre_process_landmark(
-                    landmark_list)
-                # Hand Sign Classification
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-
-
-                #Drawing on the cam
-                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
-                debug_image = draw_landmarks(debug_image, landmark_list)
-                debug_image = draw_info_text(
-                    debug_image,
-                    brect,
-                    handedness,
-                    keypoint_classifier_labels[hand_sign_id],
-                )
-        else:
-            pass
-
-        debug_image = draw_info(debug_image, fps)
-
-        # Screen Name
-        cv.imshow('Hand Gesture Recognition', debug_image)
-    cap.release()
-    cv.destroyAllWindows()
 
 def calc_bounding_rect(image, landmarks):
     image_width, image_height = image.shape[1], image.shape[0]
@@ -339,20 +213,37 @@ def draw_info_text(image, brect, handedness, hand_sign_text):
       
     return image
     
-def draw_info(image, fps):
+def draw_info(image, fps, number, mode):
     cv.putText(image, "FPS:" + str(fps), (10,30), cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 4, cv.LINE_AA)
-    cv.putText(image, "FPS:" + str(fps), (10,30), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2, cv.LINE_AA)
-
+    cv.putText(image, "FPS:" + str(fps), (10,30), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2, cv.LINE_AA)          
+    if mode==0:
+          cv.putText(image, "MODE: Static Gestures Demo", (10, 90),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv.LINE_AA)
+    if mode==1:
+          cv.putText(image, "MODE: Static Gesture Data Collection", (10, 90),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv.LINE_AA)
+    if mode==2:
+          cv.putText(image, "MODE: Not decided yet", (10, 90),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
+                   cv.LINE_AA)
     return image
-if __name__ == '__main__':
-    main()
 
+def select_mode(key, mode):
+    number = -1
+    if 48 <= key <= 57:
+        number = key - 48
+    if key == 110:
+        mode = 1
+    return number, mode
 
-
-
-
-
-
-
-
-
+def write_in_csv(number, mode, landmark_list):
+    if mode == 1 and (0 <= number <= 9):
+        dataset_path = 'datasets\keypoint.csv'
+        with open(dataset_path, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([number, *landmark_list])
+    if mode == 2:
+        #later for motion sensing of the hand
+        pass
